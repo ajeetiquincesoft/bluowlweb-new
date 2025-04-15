5fb25d3616859bb9fc94d72b21bc12e2d4e2634d
mohit <mohitpachauri82@gmail.com>
2025-04-15 17:17:43 +0530

    map design change

--- resources/views/vendor-details-page.blade.php
+++ resources/views/vendor-details-page.blade.php
@@ -369,21 +369,21 @@
                                         <div style="min-width: 300px;">
                                             <iframe
                                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2797.272592467303!2d77.65388447424512!3d27.49359363488905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x397371610b6bbc8b%3A0x97268e8ea51f8590!2siQuinceSoft!5e1!3m2!1sen!2sin!4v1744711459113!5m2!1sen!2sin"
-                                                width="100%" height="300" style="border:0;" allowfullscreen=""
+                                                width="100%" height="200" style="border:0;" allowfullscreen=""
                                                 loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                         </div>
 
                                         <!-- Location 2 -->
                                         <div style="min-width: 300px;">
                                             <iframe src="https://www.google.com/maps/embed?pb=YOUR_SECOND_LOCATION_URL"
-                                                width="100%" height="300" style="border:0;" allowfullscreen=""
+                                                width="100%" height="200" style="border:0;" allowfullscreen=""
                                                 loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                         </div>
 
                                         <!-- Location 3 -->
                                         <div style="min-width: 300px;">
                                             <iframe src="https://www.google.com/maps/embed?pb=YOUR_SECOND_LOCATION_URL"
-                                                width="100%" height="300" style="border:0;" allowfullscreen=""
+                                                width="100%" height="200" style="border:0;" allowfullscreen=""
                                                 loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                         </div>
 
