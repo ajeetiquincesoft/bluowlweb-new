fdfedfc856325e1bc25acf4411c41858751500b8
mohit <mohitpachauri82@gmail.com>
2025-04-21 13:56:08 +0530

    post web change

--- routes/api.php
+++ routes/api.php
@@ -50,6 +50,7 @@ Route::group(['middleware' => 'auth.jwt', 'prefix' => 'auth'], function ($router
     Route::POST('getHelpData', [MasterApiController::class, 'getHelpData']);
     Route::GET('getNotificationData', [MasterApiController::class, 'getNotificationData']);
     Route::POST('updateNotification', [MasterApiController::class, 'updateNotification']);
+    Route::POST('addVendorArea', [MasterApiController::class, 'addVendorArea']);
 });
 // Route::post('login', [UserController::class, 'login']);
 // Route::post('logout', [UserController::class, 'logout']);
