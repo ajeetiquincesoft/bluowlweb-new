f3a5c076b3d5ac8f4d71464fc9c6e5bda2c738f2
mohit <mohitpachauri82@gmail.com>
2025-04-24 12:20:38 +0530

    spelling Correct

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
                                             </span>
                                         </div>
                                         <div class="flex-grow-1 overflow-hidden ms-3">
-                                            <p class="text-uppercase fw-medium text-muted text-truncate mb-3">Totel
+                                            <p class="text-uppercase fw-medium text-muted text-truncate mb-3">Total
                                                 Customers
                                             </p>
                                             <div class="d-flex align-items-center mb-3">
@@ -42,7 +42,7 @@
                                             </span>
                                         </div>
                                         <div class="flex-grow-1 ms-3">
-                                            <p class="text-uppercase fw-medium text-muted mb-3">Totel Vendors</p>
+                                            <p class="text-uppercase fw-medium text-muted mb-3">Total Vendors</p>
                                             <div class="d-flex align-items-center mb-3">
                                                 <h4 class="fs-4 flex-grow-1 mb-0"><span class="counter-value"
                                                         data-target="{{$vendor_count}}">0</span></h4>
