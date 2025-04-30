1547392a2c5ba46b0479827ccd6706aff4a827b1
mohit <mohitpachauri82@gmail.com>
2025-04-30 10:53:31 +0530

    Order 2 is not comming and order id is desc

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,11 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Order extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    protected static function booted()
+    {
+        // Exclude records where status is 4
+        static::addGlobalScope('excludeStatus4', function (Builder $builder) {
+            $builder->where('status', '!=', 2);
+        });
+
+        // Order records by `created_at` in descending order
+        static::addGlobalScope('orderByCreatedAtDesc', function (Builder $builder) {
+            $builder->orderBy('created_at', 'desc');
+        });
+    }
     public function OrderitemDartaWithOrder()
     {
         return $this->hasMany(OrderItem::class,'order_id','id');
