f0b69ebcd420475a97b4d07f1358483483890d71
mohit <mohitpachauri82@gmail.com>
2025-05-03 18:10:04 +0530

    change

--- app/Http/Controllers/MasterApiController.php
+++ app/Http/Controllers/MasterApiController.php
@@ -406,7 +406,7 @@ class MasterApiController extends Controller
                 'services_category' => $services_category,
                 'sub_data' => $sub_data,
                 'isSubscribe' => false,
-                'isPaymentShow'=>1,
+                'isPaymentShow'=>0,
                 'message' => 'User Data retrieved successfully.',
                 'success' => true,
             ]);
@@ -422,7 +422,7 @@ class MasterApiController extends Controller
             'services_category' => $services_category,
             'sub_data' => $sub_data,
             'isSubscribe' => $status,
-            'isPaymentShow'=>1,
+            'isPaymentShow'=>0,
             'message' => 'User Data retrieved successfully.',
             'success' => true,
         ]);
